3c42f333eb89c116b4e51cbbce55bfb07c223515
fp024 <aopenasus@naver.com>
2022-03-20 04:20:40 +0900

    로그 보기 스크립트 추가, 먼저 커밋에서 경로 환경변수 잘못 사용한 부분 수정

--- start-server.sh
+++ start-server.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 HOME_DIR=~/git-fp024/learning-spring-web-project-by-code
 export $(grep -v '^#' $HOME_DIR/env.properties | xargs)
-cd $HOME_DIR/$LATEST
-nohup $HOME_DIR/latest/mvnw clean jetty:run 1>$HOME_DIR/log/board.log 2>&1 &
+cd $HOME_DIR/$LATEST_HOME
+nohup $HOME_DIR/$LATEST_HOME/mvnw clean jetty:run 1>$HOME_DIR/log/board.log 2>&1 &
