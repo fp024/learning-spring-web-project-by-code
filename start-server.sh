ed214c1dccca1a9d93b31f11ee16f24ac21267ce
fp024 <aopenasus@naver.com>
2022-03-20 04:11:23 +0900

    가장 최근 진행 완료한 프로젝트 디렉토리 환경변수 설정
    Linux 가상머신에서 쉘로 프로젝트 시작할 때, 가장 최근 진행 완료한 프로젝트 디렉토리를 보고,
    프로젝트 서버를 시작하도록 변경.
    
    원래는 심볼릭링크를 만들어줬었는데, 환경변수로 지정하는 방법이 낫겠다.

--- start-server.sh
+++ start-server.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 HOME_DIR=~/git-fp024/learning-spring-web-project-by-code
-cd $HOME_DIR/latest
 export $(grep -v '^#' $HOME_DIR/env.properties | xargs)
+cd $HOME_DIR/$LATEST
 nohup $HOME_DIR/latest/mvnw clean jetty:run 1>$HOME_DIR/log/board.log 2>&1 &
