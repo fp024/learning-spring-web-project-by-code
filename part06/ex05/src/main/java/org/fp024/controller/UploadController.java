d04812008f68cfcc54bc4a9c014ffcba51803a13
fp024 <aopenasus@naver.com>
2022-04-18 01:45:07 +0900

    그동안 업로드가 안되었던 것이 아니고, 내부적으로 자동 클린업이 되서 그런 것이였다.
    transferTo()로 처음 생성한 파일은 (메모리 -> 파일저장) 메서드가 끝날때 자동 정리되는 것 같다.
    
    StandardServletMultipartResolver 클래스의 cleanupMultipart 메서드 참조바람!!!
    
    파일이 업로드 된 이후 뭔가 다른 처리를 해줘야하는 것을 권고하는 것 같은데..
    이 프로젝트는 테스트 동작 확인용이여서, 최초 생성할 때는 tmp_파일명으로 생성후 이후 tmp_를 제거하는 식으로
    이름만 바꾸어줬다.
    
    web.xml의 <multipart-config> 이하 <location> 설정은...
    <file-size-threshold> 를 초과했을 때, 임시로 사용할 디스크 경로라고 하는데,
    동시에 2MB씩 10개 이상 파일이 업로드 시도 되었을 때에 한해서, 해당 디렉토리에 임시 파일이 생성되는 것을
    볼 수 있을 것 같긴하다.

--- part06/ex05/src/main/java/org/fp024/controller/UploadController.java
+++ part06/ex05/src/main/java/org/fp024/controller/UploadController.java
@@ -30,12 +30,25 @@ public class UploadController {
       LOGGER.info("Upload File Name: {}", multipartFile.getOriginalFilename());
       LOGGER.info("Upload File Size: {}", multipartFile.getSize());
 
-      File saveFile = new File(uploadFolder, multipartFile.getOriginalFilename());
+      File saveFile = new File(uploadFolder, "tmp_" + multipartFile.getOriginalFilename());
 
       try {
         multipartFile.transferTo(saveFile);
+        // transferTo()로 처음 생성한 파일은 (메모리 -> 파일저장) 메서드가 끝날때 자동 정리되는 것 같다.
+        //
+        // StandardServletMultipartResolver 클래스의 cleanupMultipart 메서드 참조바람!!!
+        //
+        // 파일이 업로드 된 이후 뭔가 다른 처리를 해줘야하는 것을 권고하는 것 같은데..
+        // 이 프로젝트는 테스트 동작 확인용이여서, 최초 생성할 때는 tmp_파일명으로 생성후 이후 tmp_를 제거하는 식으로
+        // 이름만 바꾸어줬다.
+        //
+        // web.xml의 <multipart-config> 이하 <location> 설정은...
+        // <file-size-threshold> 를 초과했을 때, 임시로 사용할 디스크 경로라고 하는데,
+        // 동시에 2MB씩 10개 이상 파일이 업로드 시도 되었을 때에 한해서, 해당 디렉토리에 임시 파일이 생성되는 것을
+        // 볼 수 있을 것 같긴하다.
+        //
+        saveFile.renameTo(new File(uploadFolder, multipartFile.getOriginalFilename()));
       } catch (Exception e) {
-
         LOGGER.error(e.getMessage(), e);
       }
     }
