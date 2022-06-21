a1a55c92b305c48801723aac605280906378b8dc
fp024 <aopenasus@naver.com>
2022-06-21 09:40:33 +0900

    서버 시작할 때 로그 바로 띄우자.
    그게 편하겠다.

--- start-server.sh
+++ start-server.sh
@@ -3,3 +3,4 @@ HOME_DIR=~/git-fp024/learning-spring-web-project-by-code
 export $(grep -v '^#' $HOME_DIR/env.properties | xargs)
 cd $HOME_DIR/$LATEST_HOME
 nohup $HOME_DIR/$LATEST_HOME/mvnw clean jetty:run 1>$HOME_DIR/log/board.log 2>&1 &
+$HOME_DIR/show-log.sh
