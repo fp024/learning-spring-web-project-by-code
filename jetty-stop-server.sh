46f46e18c3271273886e2bd730ab73bd405cbe9d
fp024 <aopenasus@naver.com>
2024-02-12 18:18:46 +0900

    리눅스 환경 실행 코드 정리

--- jetty-stop-server.sh
+++ jetty-stop-server.sh
@@ -2,4 +2,4 @@
 PROJECT_ROOT=~/git-fp024/learning-spring-web-project-by-code
 export $(grep -v '^#' ${PROJECT_ROOT}/setenv.properties | xargs)
 cd ${PROJECT_ROOT}/${LATEST_PROJECT_HOME}
-./mvnw jetty:stop
+./mvnw jetty:stop -Pjetty-run
