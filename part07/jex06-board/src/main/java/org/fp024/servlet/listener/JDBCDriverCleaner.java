4a0f7f8f5f6d6dc0f96b383092b250a5d0b6ab32
fp024 <aopenasus@naver.com>
2026-05-01 22:54:12 +0900

    part07의 게시판 프로젝트들에도 적용 (#41)
    web.xml 기반의 ex06-board에도 적용해서 동작을 확인했다.

---
+++ part07/jex06-board/src/main/java/org/fp024/servlet/listener/JDBCDriverCleaner.java
@@ -0,0 +1,58 @@
+package org.fp024.servlet.listener;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Collections;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@WebListener
+public class JDBCDriverCleaner implements ServletContextListener {
+  protected void deregisterJdbcDrivers(ServletContext servletContext) {
+    Collections.list(DriverManager.getDrivers())
+        .forEach(
+            driver -> {
+              ClassLoader driverClassLoader = driver.getClass().getClassLoader();
+              ClassLoader contextClassLoader = servletContext.getClassLoader();
+              ClassLoader parentClassLoader =
+                  contextClassLoader != null ? contextClassLoader.getParent() : null;
+
+              LOGGER.debug(
+                  "### {} 드라이버의 클래스 로더: {}",
+                  driver.getClass().getCanonicalName(),
+                  driverClassLoader != null
+                      ? driverClassLoader.getClass().getCanonicalName()
+                      : "Bootstrap ClassLoader");
+              LOGGER.debug(
+                  "### 현재 서블릿 컨텍스트의 클래스 로더: {}",
+                  contextClassLoader != null
+                      ? contextClassLoader.getClass().getCanonicalName()
+                      : "Bootstrap ClassLoader");
+              LOGGER.debug(
+                  "### 현재 서블릿 컨텍스트의 부모 클래스 로더: {}",
+                  parentClassLoader != null
+                      ? parentClassLoader.getClass().getCanonicalName()
+                      : "Bootstrap ClassLoader");
+
+              if (driverClassLoader == contextClassLoader
+                  || driverClassLoader == parentClassLoader) {
+                try {
+                  DriverManager.deregisterDriver(driver);
+                  LOGGER.info("### {} 드라이버 등록 해제", driver.getClass().getCanonicalName());
+                } catch (SQLException ex) {
+                  LOGGER.warn("### {} 드라이버 등록 해제 실패", driver.getClass().getCanonicalName(), ex);
+                }
+              }
+            });
+  }
+
+  @Override
+  public void contextDestroyed(ServletContextEvent event) {
+    LOGGER.trace("contextDestroyed() 실행...");
+    deregisterJdbcDrivers(event.getServletContext());
+  }
+}
