fd238958a231925781a7a7f4e4cd721ba8247bf0
fp024 <aopenasus@naver.com>
2026-05-13 14:05:52 +0900

    PART05 - jex04-board 확인 : mybatis-generator-maven-plugin 2.0.0 버전업

--- part05/jex04-board/src/main/java/org/fp024/mapper/BoardMapper.java
+++ part05/jex04-board/src/main/java/org/fp024/mapper/BoardMapper.java
@@ -5,7 +5,6 @@ import static org.mybatis.dynamic.sql.SqlBuilder.isEqualTo;
 
 import java.util.List;
 import java.util.Optional;
-import javax.annotation.Generated;
 import org.apache.ibatis.annotations.InsertProvider;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Result;
@@ -16,14 +15,13 @@ import org.apache.ibatis.annotations.SelectProvider;
 import org.apache.ibatis.type.JdbcType;
 import org.fp024.domain.BoardVO;
 import org.mybatis.dynamic.sql.BasicColumn;
-import org.mybatis.dynamic.sql.delete.DeleteDSLCompleter;
+import org.mybatis.dynamic.sql.dsl.CountDSLCompleter;
+import org.mybatis.dynamic.sql.dsl.DeleteDSLCompleter;
+import org.mybatis.dynamic.sql.dsl.SelectDSLCompleter;
+import org.mybatis.dynamic.sql.dsl.UpdateDSL;
+import org.mybatis.dynamic.sql.dsl.UpdateDSLCompleter;
 import org.mybatis.dynamic.sql.insert.render.InsertStatementProvider;
-import org.mybatis.dynamic.sql.select.CountDSLCompleter;
-import org.mybatis.dynamic.sql.select.SelectDSLCompleter;
 import org.mybatis.dynamic.sql.select.render.SelectStatementProvider;
-import org.mybatis.dynamic.sql.update.UpdateDSL;
-import org.mybatis.dynamic.sql.update.UpdateDSLCompleter;
-import org.mybatis.dynamic.sql.update.UpdateModel;
 import org.mybatis.dynamic.sql.util.SqlProviderAdapter;
 import org.mybatis.dynamic.sql.util.mybatis3.CommonCountMapper;
 import org.mybatis.dynamic.sql.util.mybatis3.CommonDeleteMapper;
@@ -32,15 +30,12 @@ import org.mybatis.dynamic.sql.util.mybatis3.MyBatis3Utils;
 
 @Mapper
 public interface BoardMapper extends CommonCountMapper, CommonDeleteMapper, CommonUpdateMapper {
-    @Generated("org.mybatis.generator.api.MyBatisGenerator")
     BasicColumn[] selectList = BasicColumn.columnList(bno, title, content, writer, regdate, updateDate, replyCount);
 
-    @Generated("org.mybatis.generator.api.MyBatisGenerator")
     @InsertProvider(type=SqlProviderAdapter.class, method="insert")
     @SelectKey(statement="SELECT seq_board.nextval FROM DUAL", keyProperty="row.bno", before=true, resultType=Long.class)
     int insert(InsertStatementProvider<BoardVO> insertStatement);
 
-    @Generated("org.mybatis.generator.api.MyBatisGenerator")
     @SelectProvider(type=SqlProviderAdapter.class, method="select")
     @Results(id="BoardVOResult", value = {
         @Result(column="BNO", property="bno", jdbcType=JdbcType.BIGINT, id=true),
@@ -53,83 +48,71 @@ public interface BoardMapper extends CommonCountMapper, CommonDeleteMapper, Comm
     })
     List<BoardVO> selectMany(SelectStatementProvider selectStatement);
 
-    @Generated("org.mybatis.generator.api.MyBatisGenerator")
     @SelectProvider(type=SqlProviderAdapter.class, method="select")
     @ResultMap("BoardVOResult")
     Optional<BoardVO> selectOne(SelectStatementProvider selectStatement);
 
-    @Generated("org.mybatis.generator.api.MyBatisGenerator")
     default long count(CountDSLCompleter completer) {
         return MyBatis3Utils.countFrom(this::count, boardVO, completer);
     }
 
-    @Generated("org.mybatis.generator.api.MyBatisGenerator")
     default int delete(DeleteDSLCompleter completer) {
         return MyBatis3Utils.deleteFrom(this::delete, boardVO, completer);
     }
 
-    @Generated("org.mybatis.generator.api.MyBatisGenerator")
     default int deleteByPrimaryKey(Long bno_) {
         return delete(c -> 
             c.where(bno, isEqualTo(bno_))
         );
     }
 
-    @Generated("org.mybatis.generator.api.MyBatisGenerator")
     default int insert(BoardVO row) {
         return MyBatis3Utils.insert(this::insert, row, boardVO, c ->
-            c.map(bno).toProperty("bno")
-            .map(title).toProperty("title")
-            .map(content).toProperty("content")
-            .map(writer).toProperty("writer")
-            .map(regdate).toProperty("regdate")
-            .map(updateDate).toProperty("updateDate")
-            .map(replyCount).toProperty("replyCount")
+            c.withMappedColumn(bno)
+            .withMappedColumn(title)
+            .withMappedColumn(content)
+            .withMappedColumn(writer)
+            .withMappedColumn(regdate)
+            .withMappedColumn(updateDate)
+            .withMappedColumn(replyCount)
         );
     }
 
-    @Generated("org.mybatis.generator.api.MyBatisGenerator")
     default int insertSelective(BoardVO row) {
         return MyBatis3Utils.insert(this::insert, row, boardVO, c ->
-            c.map(bno).toProperty("bno")
-            .map(title).toPropertyWhenPresent("title", row::getTitle)
-            .map(content).toPropertyWhenPresent("content", row::getContent)
-            .map(writer).toPropertyWhenPresent("writer", row::getWriter)
-            .map(regdate).toPropertyWhenPresent("regdate", row::getRegdate)
-            .map(updateDate).toPropertyWhenPresent("updateDate", row::getUpdateDate)
-            .map(replyCount).toPropertyWhenPresent("replyCount", row::getReplyCount)
+            c.withMappedColumn(bno)
+            .withMappedColumnWhenPresent(title, row::getTitle)
+            .withMappedColumnWhenPresent(content, row::getContent)
+            .withMappedColumnWhenPresent(writer, row::getWriter)
+            .withMappedColumnWhenPresent(regdate, row::getRegdate)
+            .withMappedColumnWhenPresent(updateDate, row::getUpdateDate)
+            .withMappedColumnWhenPresent(replyCount, row::getReplyCount)
         );
     }
 
-    @Generated("org.mybatis.generator.api.MyBatisGenerator")
     default Optional<BoardVO> selectOne(SelectDSLCompleter completer) {
         return MyBatis3Utils.selectOne(this::selectOne, selectList, boardVO, completer);
     }
 
-    @Generated("org.mybatis.generator.api.MyBatisGenerator")
     default List<BoardVO> select(SelectDSLCompleter completer) {
         return MyBatis3Utils.selectList(this::selectMany, selectList, boardVO, completer);
     }
 
-    @Generated("org.mybatis.generator.api.MyBatisGenerator")
     default List<BoardVO> selectDistinct(SelectDSLCompleter completer) {
         return MyBatis3Utils.selectDistinct(this::selectMany, selectList, boardVO, completer);
     }
 
-    @Generated("org.mybatis.generator.api.MyBatisGenerator")
     default Optional<BoardVO> selectByPrimaryKey(Long bno_) {
         return selectOne(c ->
             c.where(bno, isEqualTo(bno_))
         );
     }
 
-    @Generated("org.mybatis.generator.api.MyBatisGenerator")
     default int update(UpdateDSLCompleter completer) {
         return MyBatis3Utils.update(this::update, boardVO, completer);
     }
 
-    @Generated("org.mybatis.generator.api.MyBatisGenerator")
-    static UpdateDSL<UpdateModel> updateAllColumns(BoardVO row, UpdateDSL<UpdateModel> dsl) {
+    static UpdateDSL updateAllColumns(BoardVO row, UpdateDSL dsl) {
         return dsl.set(bno).equalTo(row::getBno)
                 .set(title).equalTo(row::getTitle)
                 .set(content).equalTo(row::getContent)
@@ -139,8 +122,7 @@ public interface BoardMapper extends CommonCountMapper, CommonDeleteMapper, Comm
                 .set(replyCount).equalTo(row::getReplyCount);
     }
 
-    @Generated("org.mybatis.generator.api.MyBatisGenerator")
-    static UpdateDSL<UpdateModel> updateSelectiveColumns(BoardVO row, UpdateDSL<UpdateModel> dsl) {
+    static UpdateDSL updateSelectiveColumns(BoardVO row, UpdateDSL dsl) {
         return dsl.set(bno).equalToWhenPresent(row::getBno)
                 .set(title).equalToWhenPresent(row::getTitle)
                 .set(content).equalToWhenPresent(row::getContent)
@@ -150,7 +132,6 @@ public interface BoardMapper extends CommonCountMapper, CommonDeleteMapper, Comm
                 .set(replyCount).equalToWhenPresent(row::getReplyCount);
     }
 
-    @Generated("org.mybatis.generator.api.MyBatisGenerator")
     default int updateByPrimaryKey(BoardVO row) {
         return update(c ->
             c.set(title).equalTo(row::getTitle)
@@ -163,7 +144,6 @@ public interface BoardMapper extends CommonCountMapper, CommonDeleteMapper, Comm
         );
     }
 
-    @Generated("org.mybatis.generator.api.MyBatisGenerator")
     default int updateByPrimaryKeySelective(BoardVO row) {
         return update(c ->
             c.set(title).equalToWhenPresent(row::getTitle)
